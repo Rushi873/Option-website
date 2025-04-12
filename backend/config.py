3de161faee8efbdb8dd08227eeea56bc85a13f95
Rushi Glasswala <rushiglasswala@iitgn.ac.in>
2025-04-13 00:46:40 +0530

    correct python dict format

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,8 @@
 DB_CONFIG = {
-    host="metro.proxy.rlwy.net",
-    user="root",
-    database="option_data",
-    port=20336
+    "host": "metro.proxy.rlwy.net",
+    "user": "root",
+    "database": "option_data",
+    "port": 20336
 }
 
 # DB_CONFIG = {
