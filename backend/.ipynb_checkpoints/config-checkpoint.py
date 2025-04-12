0fadf4a873950811b408233e00c9dcc4e87a2c95
Rushi Glasswala <rushiglasswala@iitgn.ac.in>
2025-04-12 17:30:03 +0530

    Clean up repo with .gitignore

--- backend/.ipynb_checkpoints/config-checkpoint.py
+++
@@ -1,6 +0,0 @@
-DB_CONFIG = {
-    "host": "localhost",
-    "user": "root",
-    "password": "Solar@345",
-    "database": "option_data"
-}
--- frontend/.ipynb_checkpoints/plotly_chart-checkpoint.js
+++
@@ -1,9 +0,0 @@
-function updatePayoffChart() {
-    let data = [{
-        x: [17500, 18000, 18500, 19000], 
-        y: [-1000, 500, 2000, 3500], 
-        type: 'scatter'
-    }];
-    Plotly.newPlot("payoff-chart", data);
-}
-updatePayoffChart();
