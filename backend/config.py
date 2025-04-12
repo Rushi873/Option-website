8ef40e1ed6b06aa839db3938fe1e9004ff63d5ed
Rushi Glasswala <rushiglasswala@iitgn.ac.in>
2025-04-13 00:10:07 +0530

    use env var for db connection

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,13 @@
 DB_CONFIG = {
-    "host": "localhost",
-    "user": "root",
-    "password": "Solar@345",
-    "database": "option_data"
+    host="metro.proxy.rlwy.net",
+    user="root",
+    database="railway",
+    port=20336
 }
+
+# DB_CONFIG = {
+#     "host": "localhost",
+#     "user": "root",
+#     "password": "Solar@345",
+#     "database": "option_data"
+# }
--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,13 @@
+from dotenv import load_dotenv
+
 import mysql.connector
 from config import DB_CONFIG
 
+# Load from .env file (if using one)
+load_dotenv()
+
+password = os.getenv("MYSQL_PASSWORD")
+
 def get_db_connection():
+    DB_CONFIG["password"] = password
     return mysql.connector.connect(**DB_CONFIG)
