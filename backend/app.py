8d0b136058ee4b572528352d48450ec298fb0d6f
Rushi Glasswala <rushiglasswala@iitgn.ac.in>
2025-04-12 22:35:55 +0530

    correct parent package name

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 import sys
 from pathlib import Path
-sys.path.append(str(Path(__file__).parent.parent.parent))
+sys.path.append(str(Path(__file__).parent))
 
 from fastapi import FastAPI, HTTPException, Query, Body
 from pydantic import BaseModel
